ccb6878f4e9d7dcb7411f1fb274900256dad7eef
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-04 17:36:49 -0400

    Set up sign up page styling

--- client/src/auth/Auth.js
+++ client/src/auth/Auth.js
@@ -1,7 +1,7 @@
 //==============================================================================
 //Import Packages Required For App
 //==============================================================================
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 //==============================================================================
