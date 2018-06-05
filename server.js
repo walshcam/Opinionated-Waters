4f58cd33c066cd36410262652a65484ec0ca0e09
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-05 10:31:56 -0400

    Add CORS for testing purposes

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-//********** DEPENDENCIES **********
+//==============================================================================
+//Dependencies
+//==============================================================================
 
 const express = require("express");
 const http = require("http");
@@ -6,6 +8,8 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 const mongoose = require('mongoose');
+//CORS is used for testing purposes
+const cors = require('cors');
 
 // DB Setup - Require All Models
 const db = require('./models');
@@ -18,7 +22,9 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/auth";
 mongoose.connect(MONGODB_URI);
 
 
-//*********** APP SETUP *********
+//==============================================================================
+//App Setup
+//==============================================================================
 
 //app.use registers middleware
 //morgan is a logging framework
@@ -26,13 +32,19 @@ app.use(morgan('combined'));
 //bodyParser parses requests into json
 app.use(bodyParser.json({ type: '*/*' }));
 
-//***** Routes *****
+//Use CORS for testing purposes
+app.use(cors());
+
+//==============================================================================
+//Routes
+//==============================================================================
 
 require("./routes/html-routes.js")(app);
 require("./routes/authentication-routes.js")(app);
 
-
-//*********** SERVER SETUP ***********
+//==============================================================================
+//Server Setup
+//==============================================================================
 
 //Specify Port
 const PORT = process.env.PORT || 3001;
@@ -40,30 +52,3 @@ const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 server.listen(PORT);
 console.log('Server listening on:', PORT);
-
-
-
-
-
-
-
-
-// // Define middleware here
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// // Define API routes here
-
-// // Send every other request to the React app
-// // Define any API routes before this runs
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`🌎 ==> Server now on port ${PORT}!`);
-// });
