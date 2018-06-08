ab071f116783e8eb30474427eda7ea6126921e25
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-08 14:31:26 -0400

    Try this?

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -10,11 +10,11 @@ const requireAuth = passport.authenticate('jwt', { session: false });
 //Post route for sending and receiving authentication
 module.exports = function(app) {
     //require authentication before ('/') get request with requireAuth
-    // app.get('/', requireAuth, function(req, res) {
+    // app.get('/', function(req, res) {
     //     res.send({ hi: 'there' });
     // });
     // Define any API routes before this runs
     app.get("*", function(req, res) {
-        res.sendFile(path.join(__dirname, "./../client/build/index.html"));
+        res.sendFile(path.join(__dirname, "./../client/index.js"));
     });
 }
