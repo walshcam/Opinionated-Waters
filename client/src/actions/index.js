fa0208e712a036637093f2b70ee8231efdef0f9c
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-08 17:19:07 -0400

    Alter index.js in actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,7 +9,7 @@ import { AUTH_USER, AUTH_ERROR } from './types';
 export const signin = (formProps, callback) => async dispatch => {
     try{
         //Await is used to ensure this action is taken before moving forward
-        const response = await axios.post('http://localhost:3001/signin', formProps);
+        const response = await axios.post('https://opinwater.herokuapp.com/signin', formProps);
 
         dispatch({ type: AUTH_USER, payload: response.data.token });
         //This saves the token to local storage
@@ -29,7 +29,7 @@ export const signin = (formProps, callback) => async dispatch => {
 export const signup = (formProps, callback) => async dispatch => {
     try{
         //Await is used to ensure this action is taken before moving forward
-        const response = await axios.post('http://localhost:3001/signup', formProps);
+        const response = await axios.post('https://opinwater.herokuapp.com/signup', formProps);
 
         dispatch({ type: AUTH_USER, payload: response.data.token });
         //This saves the token to local storage
