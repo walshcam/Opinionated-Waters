9e0427437354dcecf07d71655fbcd4015224c964
walshcam <35083171+walshcam@users.noreply.github.com>
2018-06-11 20:53:26 -0400

    Fixing Media Screen

--- client/src/components/UI/MapComponent/MapComponent.js
+++ client/src/components/UI/MapComponent/MapComponent.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./MapComponent.css";
 
+const mapStyle = {
+    height: '80vh'
+};
+
 const mapClass = "box";
 
 const MapComponent = ({ children }) => (
-    <div id="mapid" className = {mapClass}>
+    <div id="mapid" className = {mapClass} style = {mapStyle}>
         {children}
     </div>
 );
